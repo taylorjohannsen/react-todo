a45b8170a1dff52fa921112301a30c4a75382e61
Taylor Johannsen <freakshow_tj@yahoo.com>
2018-12-26 16:28:15 -0800

    Setting up Component files

---
+++ src/Components/Todo.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+class Todo extends Component {
+
+  render() {
+    return (
+      <div>
+        <h3>Latest Projects</h3>
+      </div>
+    );
+  }
+}
+
+export default Todo;
