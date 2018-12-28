27618250c80dbd1b8144efe4bf6a3ba3d4996e50
Taylor Johannsen <freakshow_tj@yahoo.com>
2018-12-28 12:41:15 -0800

    Updates

---
+++ precache-manifest.5e33868b54453f4591f355ac75f9d542.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "5bee33ad84f16ca2cf0f",
+    "url": "/react-todo/static/js/runtime~main.5bee33ad.js"
+  },
+  {
+    "revision": "a78ca7d77e8448472255",
+    "url": "/react-todo/static/js/main.a78ca7d7.chunk.js"
+  },
+  {
+    "revision": "539856a77dcf693239c8",
+    "url": "/react-todo/static/js/1.539856a7.chunk.js"
+  },
+  {
+    "revision": "a78ca7d77e8448472255",
+    "url": "/react-todo/static/css/main.8a51bd28.chunk.css"
+  },
+  {
+    "revision": "8a0cfd2a2e0893678bb9ed04ee5a4b1a",
+    "url": "/react-todo/index.html"
+  }
+];
